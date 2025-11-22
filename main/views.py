f1d306002c4aa94440df09a11217e534f14bb039
smoovie <smoovie.screenings@gmail.com>
2025-11-22 19:07:39 +0000

    Fix admin duplicate registration issue

--- main/views.py
+++ main/views.py
@@ -1,28 +1,6 @@
-from django.contrib import admin
-from django.utils.html import format_html
+from django.shortcuts import render
 from .models import LandingAsset
 
-
-@admin.register(LandingAsset)
-class LandingAssetAdmin(admin.ModelAdmin):
-    list_display = ("title", "thumbnail", "created_at")
-    readonly_fields = ("preview",)
-
-    def thumbnail(self, obj):
-        if obj.image:
-            return format_html(
-                '<img src="{}" width="80" style="border-radius:5px;" />',
-                obj.image.url
-            )
-        return "(No image)"
-    thumbnail.short_description = "Image"
-
-    def preview(self, obj):
-        if obj.image:
-            return format_html(
-                '<img src="{}" width="300" style="border-radius:10px;" />',
-                obj.image.url
-            )
-        return "No image uploaded"
-
-
+def home(request):
+    asset = LandingAsset.objects.last()
+    return render(request, "main/index.html", {"asset": asset})
